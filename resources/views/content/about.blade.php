bf59a5f22636588b0e659af06b141bf4d759f0b9
Julia Soloveva <julia.solovev@protonmail.ch>
2021-06-26 17:23:32 +0200

    image about added

--- resources/views/content/about.blade.php
+++ resources/views/content/about.blade.php
@@ -4,7 +4,14 @@
 @section('content')
     <link rel="stylesheet" href="{{asset('css/about.css')}}">
     <h2 id="about" class="text-center">We are a team of developers, who decided to build a new kitchen tool, which is there to help everyone to cook more at home. Since cooking at home is not only better for the environment, but also is helping to save some money. To build this tool, we used Laravel. Laravel is a web application framework with expressive, elegant syntax.</h2>
-
+    <div class="content">
+        <img src="/images/developer.svg"
+             style="
+                display: block;
+                margin-left: auto;
+                margin-right: auto;
+                width: 50%; ">
+    </div>
 
 
 @endsection
