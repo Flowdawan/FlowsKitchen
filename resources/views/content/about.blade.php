9fae94d18522c2847dcc0c7c37775113d88a5f9c
Julia Soloveva <julia.solovev@protonmail.ch>
2021-06-26 14:12:18 +0200

    text "about"

--- resources/views/content/about.blade.php
+++ resources/views/content/about.blade.php
@@ -3,7 +3,7 @@
 
 @section('content')
 
-    <h1 class="text-center">We are a team of developers</h1>
+    <h1 class="text-center">We are a team of developers, who decided to build a new kitchen tool, which is there to help everyone to cook more at home. Since cooking at home is not only better for the environment, but also is helping to save some money. To build this tool, we used Laravel. Laravel is a web application framework with expressive, elegant syntax.</h1>
 
 
 
