026e73c4ab561d9d93fa74980c705a32957b25ed
Julia Soloveva <julia.solovev@protonmail.ch>
2021-06-26 16:57:49 +0200

    css about added

--- resources/views/content/about.blade.php
+++ resources/views/content/about.blade.php
@@ -2,8 +2,8 @@
 @section('title', 'About')
 
 @section('content')
-
-    <h1 class="text-center">We are a team of developers, who decided to build a new kitchen tool, which is there to help everyone to cook more at home. Since cooking at home is not only better for the environment, but also is helping to save some money. To build this tool, we used Laravel. Laravel is a web application framework with expressive, elegant syntax.</h1>
+    <link rel="stylesheet" href="{{asset('css/about.css')}}">
+    <h2 id="about" class="text-center">We are a team of developers, who decided to build a new kitchen tool, which is there to help everyone to cook more at home. Since cooking at home is not only better for the environment, but also is helping to save some money. To build this tool, we used Laravel. Laravel is a web application framework with expressive, elegant syntax.</h2>
 
 
 
