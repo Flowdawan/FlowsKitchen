95d39f90a4d8982174d0463091d4ac58465fcb94
Flow <flo-m@gmx.at>
2021-06-28 23:41:46 +0200

    small change

--- resources/views/content/recipes.blade.php
+++ resources/views/content/recipes.blade.php
@@ -4,18 +4,6 @@
 
     <h1 class="text-center text-white">Here you can see your favorite Recipes</h1>
 
-    <form action="{{route('recipes.show')}}" method="POST">
-        @csrf
-        <input id="recipeId" type="text"
-                name="recipeId"
-               value="" autofocus>
-
-        <button class="btn-danger text-black-50" type="submit"
-                id="post_something">Post
-            Account
-        </button>
-    </form>
-
     <!--- To check with there are any posts, if yes iterate throw and show --->
     @if($recipes->count())
         <!--- api call einbauen --->
